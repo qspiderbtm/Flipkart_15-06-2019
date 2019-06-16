9cd802f93709ae692c997d23a3101cc2aa9ac4d5
qspiderbtm <51853137+qspiderbtm@users.noreply.github.com>
2019-06-16 16:00:53 +0530

    removed unwanted lines of this task
    removed unwanted lines of this task

--- testdata/data.py
+++ testdata/data.py
@@ -1,8 +1,3 @@
 PATH = 'E:\drivers/chromedriver.exe'
-# URL = 'https://www.flipkart.com/'
 URL = 'https://www.flipkart.com/'
 SearchText = 'Macbook'
-
-import inspect
-def whoami():
-    return inspect.stack()[1][3]
