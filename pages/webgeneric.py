5aa6b64bf40d66b86a381611a5a50e01f5033b7b
qspiderbtm <51853137+qspiderbtm@users.noreply.github.com>
2019-06-16 15:58:35 +0530

    removed unwanted lines of this task
    removed unwanted lines of this task

--- pages/webgeneric.py
+++ pages/webgeneric.py
@@ -18,27 +18,6 @@ class WebGeneric(LocGeneric):
         e = self.locator(locator_type, locator_val)
         e.click()
 
-    def report_pass(self):
-        pass
-
-    def report_fail(self):
-        pass
-
-    def select_day(self, locator_type, locator_val, input_val):
-        day = self.locator(locator_type, locator_val)
-        select = Select(day)
-        select.select_by_visible_text(input_val)
-
-    def select_month(self, locator_type, locator_val, input_val):
-        month = self.locator(locator_type, locator_val)
-        select = Select(month)
-        select.select_by_value(input_val)
-
-    def select_year(self, locator_type, locator_val, input_val):
-        year = self.locator(locator_type, locator_val)
-        select = Select(year)
-        select.select_by_index(input_val)
-
     def mouse_hr(self, locator_type, locator_val):
         ele= self.locator(locator_type, locator_val)
         webdriver.ActionChains(self.driver).move_to_element(ele).perform()
